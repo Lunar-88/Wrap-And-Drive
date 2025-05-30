371e8a774683e8f8e8a6870e3629092f08a24155
Felix Kemboi <felixkemboi824@gmail.com>
2025-05-30 03:22:52 +0300

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Footer></Footer>
+    </>
+  )
+  
+}
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer () {
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Laser Wraps</p>
+        </footer>
+    )
+
+}
+
+export default Footer
